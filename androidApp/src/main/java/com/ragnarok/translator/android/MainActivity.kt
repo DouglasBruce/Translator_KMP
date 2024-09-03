9ab90e6d1c27534048f6f613e1397e06383e36ca
DouglasBruce <douglasbruce96@gmail.com>
2024-09-04 00:00:40 +0100

    Enable edgeToEdge support

--- androidApp/src/main/java/com/ragnarok/translator/android/MainActivity.kt
+++ androidApp/src/main/java/com/ragnarok/translator/android/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.ragnarok.translator.android
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,13 +16,21 @@ import com.ragnarok.translator.Greeting
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                window.isNavigationBarContrastEnforced = false
+            }
             MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    GreetingView(Greeting().greet())
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        GreetingView(Greeting().greet())
+                    }
                 }
             }
         }
