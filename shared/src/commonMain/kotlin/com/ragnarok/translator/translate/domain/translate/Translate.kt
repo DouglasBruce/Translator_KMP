59fa5ca865dd328ab5ed322cb7ef79eef6188685
DouglasBruce <douglasbruce96@gmail.com>
2024-09-04 00:14:05 +0100

    Setting up translate use case

---
+++ shared/src/commonMain/kotlin/com/ragnarok/translator/translate/domain/translate/Translate.kt
@@ -0,0 +1,39 @@
+package com.ragnarok.translator.translate.domain.translate
+
+import com.ragnarok.translator.core.domain.language.Language
+import com.ragnarok.translator.core.domain.util.Resource
+import com.ragnarok.translator.translate.domain.history.HistoryDataSource
+import com.ragnarok.translator.translate.domain.history.HistoryItem
+
+class Translate(
+    private val client: TranslateClient,
+    private val historyDataSource: HistoryDataSource
+) {
+
+    suspend fun execute(
+        fromLanguage: Language,
+        fromText: String,
+        toLanguage: Language
+    ): Resource<String> {
+        return try {
+            val translatedText = client.translate(
+                fromLanguage, fromText, toLanguage
+            )
+
+            historyDataSource.insertHistoryItem(
+                HistoryItem(
+                    id = null,
+                    fromLanguageCode = fromLanguage.langCode,
+                    fromText = fromText,
+                    toLanguageCode = toLanguage.langCode,
+                    toText = translatedText,
+                )
+            )
+
+            Resource.Success(translatedText)
+        } catch (e: TranslateException) {
+            e.printStackTrace()
+            Resource.Error(e)
+        }
+    }
+}
